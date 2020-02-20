23dd9d768b72b2f5754cb40dafd8e88b2eb9b273
Ahmed <ahmedhamedaly19@gmail.com>
2020-02-20 18:49:38 +0000

    added parsing

--- hashcode-solution.py
+++ hashcode-solution.py
@@ -1,6 +1,31 @@
 import re
 import random
 
+def main():
+    dataFile = "a_example.txt"
+    with open(dataFile) as file:
+        dataLines = file.readlines()
+
+        data = [line.strip() for line in dataLines]
+
+        bkLibScan = [int(i) for i in data[0].split()]
+
+        books = bkLibScan[0]
+        libraries = bkLibScan[1]
+        daysForScan = bkLibScan[2]
+
+        scores = [int(i) for i in data[1].split()]
+
+        scoreDict = {}
+        for index in range(len(scores)):
+            scoreDict[scores[index]] = index
+
+        scoreList = [(scoreDict[score], score) for score in scoreDict]
+
+        for index, lib in enumerate(data[2:], start=1):
+            print(f"{lib} and {index}")
+            if index % 2 is 1:
+                print("hello")
 
 class library:
     def __init__(self, amount, sTime, bShip, bList):
@@ -13,11 +38,12 @@ class library:
         return 0
     
     def containsBook(self, bookId):
-        if(bList.contains(bookId)):
+        if(self.bList.contains(bookId)):
            return bookId
         return -1
     
 
-#file1 = open('a_example.in', "r")
 
-print("hey")
+
+if __name__ == '__main__':
+    main()
