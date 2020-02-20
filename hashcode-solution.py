4bdc1bcc7f1fae1e0d15209f148d68362af96910
Daniel N <nugentd2@tcd.ie>
2020-02-20 18:55:52 +0000

    lol

--- hashcode-solution.py
+++ hashcode-solution.py
@@ -1,15 +1,17 @@
 import re
 import random
 
-
+def sortBooks(books):
+     books.sort(key = lambda tuple : tuple[1], reverse = True)
 class library:
+
     def __init__(self, amount, sTime, bShip, bList):
+        sortBooks(bList)
         self.amount = amount
         self.sTime = sTime
-        self.bList = sortBooks(self, bList)
+        self.bList = bList
         self.bShip = bShip
-    def sortBooks(self, books):
-        self.bList.sort(key = lambda tuple : tuple[1])
+
 
     def containsBook(self, bookId):
         if(bList.contains(bookId)):
@@ -20,7 +22,16 @@ class library:
     
 def sortLibraries(libraries):
     libraries.sort(key=lambda lib: lib.sTime)
-    
-#file1 = open('a_example.in', "r")
 
-print("hey")
+
+lib1 = library(5,2,2, [(0, 1), (1, 2), (2, 3), (3, 6), (4, 5)])
+lib2 = library(4,3,1, [(3, 6), (2, 3), (5, 4), (0, 1)])
+lib3 = library(2,4,1, [(3, 6), (2, 3)])
+libs = [lib1, lib3, lib2]
+sortLibraries(libs)
+s = ""
+for lib in libs:
+    print(f'lib: {lib.amount}')
+    for tup in lib.bList:
+        print(tup)
+#file1 = open('a_example.in', "r")
