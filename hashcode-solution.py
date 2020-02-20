f5e793f706ccbcc2197ef6fe6b6fc9e267c8104b
Daniel N <nugentd2@tcd.ie>
2020-02-20 18:20:07 +0000

    adding libary

---
+++ hashcode-solution.py
@@ -0,0 +1,23 @@
+import re
+import random
+
+
+class library:
+    def __init__(self, amount, sTime, bShip, bList):
+        self.amount = amount
+        self.sTime = sTime
+        self.bShip = bShip
+        self.bList = bList
+    
+    def sortBooks(self, books):
+        return 0
+    
+    def containsBook(self, bookId):
+        if(bList.contains(bookId)):
+           return bookId
+        return -1
+    
+
+#file1 = open('a_example.in', "r")
+
+print("hey")
