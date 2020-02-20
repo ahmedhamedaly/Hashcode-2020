725254c45667fcd6f249246e40084a8b76930145
Daniel N <nugentd2@tcd.ie>
2020-02-20 18:39:41 +0000

    lib

--- hashcode-solution.py
+++ hashcode-solution.py
@@ -6,18 +6,21 @@ class library:
     def __init__(self, amount, sTime, bShip, bList):
         self.amount = amount
         self.sTime = sTime
+        self.bList = sortBooks(self, bList)
         self.bShip = bShip
-        self.bList = bList
-    
     def sortBooks(self, books):
-        return 0
-    
+        self.bList.sort(key = lambda tuple : tuple[1])
+
     def containsBook(self, bookId):
         if(bList.contains(bookId)):
            return bookId
         return -1
+    def removeBook(self, bookId):
+        self.bList = [i for i in self.bList if i[0] != bookId]
     
-
+def sortLibraries(libraries):
+    libraries.sort(key=lambda lib: lib.sTime)
+    
 #file1 = open('a_example.in', "r")
 
 print("hey")
